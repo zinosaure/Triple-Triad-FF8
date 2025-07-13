b9c42a28cedf107cdcdc0bc1e763f28ee14af0da
Gino D. <25200363+zinosaure@users.noreply.github.com>
2025-07-13 03:38:54 +0200

    Save wip: new ui implemented

--- src/app/models/game.py
+++ src/app/models/game.py
@@ -2,19 +2,35 @@ import json
 import random
 
 from pathlib import Path
-from typing import Optional
+from typing import Any, Optional
 
 
-class TipleTriad:
+class TripleTriad:
     Pack: dict[int, dict[str, str | int]] = {}
+
+    @staticmethod
+    def chunk(chunk_size: int = 10) -> list[dict[str, str | int]]:
+        results = []
+        new_dict = {}
+
+        for k, v in TripleTriad.Pack.items():
+            if len(new_dict) < chunk_size:
+                new_dict[k] = v
+            else:
+                results.append(new_dict)
+                new_dict = {k: v}
+
+        results.append(new_dict)
+
+        return results
 
 
 with open(Path("/app/src/app/assets/cards-data.json")) as handle:
-    TipleTriad.Pack = {data["id"] + 1: data for data in json.load(handle)}
+    TripleTriad.Pack = {data["id"] + 1: data for data in json.load(handle)}
 
 
 class Card:
-    def __init__(self, id: int):
+    def __init__(self, id: int, hand: "Hand"):
         assert 1 <= id <= 110
 
         self.name: str = ""
@@ -24,8 +40,14 @@ class Card:
         self.left: int = 0
         self.level: int = 0
         self.element: str = ""
-        self.__dict__.update(TipleTriad.Pack.get(id) or {})
+        self.__dict__.update(TripleTriad.Pack.get(id) or {})
         self.id = id
+        filename = self.name.replace(" ", "_")
+
+        if hand.POSITION == OppositeHand.POSITION:
+            self.image: str = f"/public/images/cards/a/TT_{filename}.png"
+        else:
+            self.image: str = f"/public/images/cards/b/TT_{filename}.png"
 
     def __getitem__(self, name: str) -> int:
         return getattr(self, name)
@@ -35,22 +57,29 @@ class Card:
 
 
 class Hand:
+    POSITION: int = 1
+    H1: int = 1
+    H2: int = 2
+    H3: int = 3
+    H4: int = 4
+    H5: int = 5
+
     def __init__(self, id: int, selected_cards: list[int]):
         assert id > 0
         assert len(selected_cards) == 5
 
         self.id: int = id
         self.hold_cards: dict[int, Card] = {
-            1: Card(selected_cards[0]),
-            2: Card(selected_cards[1]),
-            3: Card(selected_cards[2]),
-            4: Card(selected_cards[3]),
-            5: Card(selected_cards[4]),
+            1: Card(selected_cards[0], self),
+            2: Card(selected_cards[1], self),
+            3: Card(selected_cards[2], self),
+            4: Card(selected_cards[3], self),
+            5: Card(selected_cards[4], self),
         }
         self.unhold_cards: list[int] = []
 
     def __str__(self) -> str:
-        return f"P[{self.id}]"
+        return f"P{self.POSITION}"
 
     def is_card_available(self, hold_id: int) -> Card:
         assert 1 <= hold_id <= 5
@@ -61,15 +90,28 @@ class Hand:
         raise Exception(f"Hand[{self.id}] does not holds card: {hold_id}")
 
 
-class Game:
-    def __init__(self, uniqid: str, hand: Hand, opposite_hand: Hand):
-        assert hand.id != opposite_hand.id
+class OppositeHand(Hand):
+    POSITION: int = 2
 
+
+class Game:
+    Singleton: dict[str, "Game"] = {}
+    rule_list = [
+        "open",
+        "closed",
+        "random",
+        "same",
+        "combo",
+        "wall",
+        "plus",
+        "swap",
+        "elemental",
+        "sudden_death",
+    ]
+
+    def __init__(self, uniqid: str):
         self.uniqid: str = uniqid
-        self.hands: dict[int, Hand] = {
-            1: hand,
-            2: opposite_hand,
-        }
+        self.hands: dict[int, Hand] = {}
         self.cells: dict[int, Game.Cell] = {
             1: Game.Cell(1, 1),
             2: Game.Cell(1, 2),
@@ -81,20 +123,43 @@ class Game:
             8: Game.Cell(3, 2),
             9: Game.Cell(3, 3),
         }
-        self.hand_turn: int = 1  # random.choice([1, 2])
-        self.game_rules: list[str] = []
-        self.notifications: list[str] = []
+        self.state: Game.State = Game.State()
+        Game.Singleton[uniqid] = self
 
     def __getitem__(self, cell_id: int) -> "Game.Cell":
         assert 1 <= cell_id <= 9
 
         return self.cells.get(cell_id)  # type: ignore
 
+    @staticmethod
+    def load(uniqid: str) -> "Game":
+        if uniqid not in Game.Singleton:
+            Game.Singleton[uniqid] = Game(uniqid)
+
+        return Game.Singleton[uniqid]
+
+    def set_hand(self, hand: Hand):
+        assert Hand.POSITION not in self.hands
+
+        self.hands[Hand.POSITION] = hand
+
+    def update(self) -> dict[str, Any]:
+        if self.state.status == "setup":
+            if Hand.POSITION in self.hands and OppositeHand.POSITION in self.hands:
+                self.state.status = "started"
+                self.state.hand_turn = random.choice(
+                    [Hand.POSITION, OppositeHand.POSITION]
+                )
+        else:
+            self.state.status = "setup"
+
+        return self.state.send()
+
     def move(self, *, cell_id: int, hand_id: int, hold_id: int):
         assert 1 <= cell_id <= 9
         assert 1 <= hold_id <= 5
-        assert hand_id in [1, 2]
-        assert self.hand_turn == hand_id
+        assert hand_id in [Hand.POSITION, OppositeHand.POSITION]
+        assert self.state.hand_turn == hand_id
 
         if (cell := self.cells.get(cell_id)) and (hand := self.hands.get(hand_id)):
             if cell.is_occupied():
@@ -103,11 +168,15 @@ class Game:
             if card := hand.is_card_available(hold_id):
                 cell.card = card
                 cell.hand = hand
-                self.hand_turn = 1 if self.hand_turn == 2 else 2
-                self.notifications.append(
+                self.state.notifications.append(
                     f"{cell.hand}: Placed {cell.card} in cell: {cell_id}"
                 )
-                return self.capture_cells(cell)
+                self.capture_cells(cell)
+
+                if hand_id != Hand.POSITION:
+                    self.state.hand_turn = Hand.POSITION
+                else:
+                    self.state.hand_turn = OppositeHand.POSITION
 
     def bot_move(self):
         pass
@@ -124,11 +193,19 @@ class Game:
                     if opp_cell.hand and opp_cell.card:
                         if cell.card[side] > opp_cell.card[opp_side]:
                             opp_cell.hand = cell.hand
-                            self.notifications.append(
+
+                            if self.state.hand_turn == Hand.POSITION:
+                                self.state.score_board[Hand.POSITION] += 1
+                                self.state.score_board[OppositeHand.POSITION] -= 1
+                            else:
+                                self.state.score_board[OppositeHand.POSITION] += 1
+                                self.state.score_board[Hand.POSITION] -= 1
+
+                            self.state.notifications.append(
                                 f"{cell.card.name}[{side}={cell.card[side]}] > {opp_cell.card.name}[{opp_side}={opp_cell.card[opp_side]}]"
                             )
 
-                            if "combo" in self.game_rules:
+                            if "combo" in self.state.current_rules:
                                 return self.capture_cells(opp_cell)
 
     class Cell:
@@ -166,3 +243,17 @@ class Game:
                             if 0 <= next_row < 3 and 0 <= next_column < 3:
                                 items.append((side, (cell_id, opp_side)))
             return items
+
+    class State:
+        def __init__(self):
+            self.status: str = "setup"
+            self.hand_turn: int = -1
+            self.score_board: dict[int, int] = {
+                Hand.POSITION: 5,
+                OppositeHand.POSITION: 5,
+            }
+            self.current_rules: list[str] = []
+            self.notifications: list[str] = []
+
+        def send(self) -> dict[str, Any]:
+            return self.__dict__
--- src/main.py
+++ src/main.py
@@ -3,18 +3,17 @@ import sys
 import json
 import uuid
 import time
-import random
 import asyncio
 
 from typing import Any
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, HTTPException
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request, Response
 from fastapi.websockets import WebSocketState
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from jinja2 import Environment, FileSystemLoader
 
-from app.models.game import Hand, Game
+from app.models.game import TripleTriad, Hand, Game
 
 # FastAPI
 app = FastAPI(title="Triple Triad - FF8")
@@ -32,30 +31,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-h1: Hand = Hand(110001, [80, 20, 110, 98, 99])
-h2: Hand = Hand(100022, [88, 70, 103, 101, 100])
-game: Game = Game("xxxx-yyyyyy-zzzz", h1, h2)
-game.move(cell_id=1, hand_id=1, hold_id=1)
-game.move(cell_id=2, hand_id=2, hold_id=5)
-game.move(cell_id=5, hand_id=1, hold_id=5)
-game.move(cell_id=6, hand_id=2, hold_id=4)
-
-
-for name, cell in game.cells.items():
-    if cell.is_occupied():
-        print(name, ":", cell.hand, cell.card)
-    else:
-        print(name, ":")
-
-for notification in game.notifications:
-    print(notification)
-
-CARDS: list[dict[int, int | str]] = []
-GAMES: dict[str, dict[str, Any]] = {}
-
-with open("/app/src/app/assets/cards-data.json") as handle:
-    for item in json.load(handle):
-        CARDS.append(item)
 
 connections = set()
 loop = asyncio.get_event_loop()
@@ -63,28 +38,58 @@ loop = asyncio.get_event_loop()
 
 @app.get("/")
 def home(request: Request):
-    context = {"v": time.time()}
+    context = {
+        "v": time.time(),
+        "game_id": str(uuid.uuid4()),
+        "select_modes": {
+            "bot_lvl_1": "Bot level 1",
+            "bot_lvl_2": "Bot level 2",
+            "bot_lvl_3": "Bot level 3",
+            "bot_lvl_4": "Bot level 4",
+            "bot_lvl_5": "Bot level 5",
+            "human": "Friend — send the invitation link",
+        },
+        "selection_rules": {
+            "random": "Random",
+        },
+        "game_rules": {
+            "open": "Open",
+            "elemental": "Elemental",
+            "combo": "Combo",
+            "same": "Same",
+            "same_wall": "Same Wall",
+            "plus": "Plus",
+            "plus_wall": "Plus Wall",
+        },
+        "draw_rules": {
+            "swap": "Swap",
+            "sudden_death": "Sudden Death",
+        },
+    }
+
     return HTMLResponse(
-        status_code=200, content=env.get_template("index.html").render(context)
+        status_code=200, content=env.get_template("pages/index.html").render(context)
     )
 
 
-def get_random_cards(color: str = "a"):
-    selected_cards = []
-    for i in range(5):
-        card: dict[str, Any] = random.choice(CARDS)
-        card["image"] = (
-            f"/public/images/cards/{color}/TT_{card['name'].replace(' ', '_')}.png"
-        )
-        selected_cards.append(card)
-    return selected_cards
+@app.post("/select_cards")
+def select_cards(request: Request, response: Response):
+    context = {
+        "v": time.time(),
+        "game_id": str(uuid.uuid4()),
+        "selections": TripleTriad.chunk(11),
+    }
+
+    return HTMLResponse(
+        status_code=200, content=env.get_template("pages/select_cards.html").render(context)
+    )
 
 
 @app.websocket("/ws")
-async def websocket(websocket: WebSocket, game_id: str = ""):
+async def websocket(websocket: WebSocket, game_id: str):
     try:
+        game: Game = Game.load(game_id)
         await websocket.accept()
-        websocket_id = id(websocket)
 
         if len(connections) >= 15:
             return
@@ -99,44 +104,44 @@ async def websocket(websocket: WebSocket, game_id: str = ""):
             try:
                 payload = await websocket.receive_json()
 
-                if action := payload.get("action"):
-                    if action == "setup" or game_id not in GAMES:
-                        game_id = str(uuid.uuid4())
-                        GAMES[game_id] = {
-                            "type": "reload",
-                            "game_id": game_id,
-                            "websocket_id": websocket_id,
-                            "board": {
-                                "rules": {},
-                                "hold": {
-                                    "a": get_random_cards("a"),
-                                    "b": get_random_cards("b"),
-                                },
-                                "ondeck": [],
-                                "score": {
-                                    "a": 0,
-                                    "b": 0,
-                                },
-                                "turn": random.choice(["B", "R"]),
-                            },
-                        }
-                    elif game_id in GAMES:
-                        if action == "start-game":
-                            GAMES[game_id]["type"] = "distribute"
-                        elif action == "next_turn_a":
-                            GAMES[game_id]["type"] = "prev_move_a"
-                            print(payload)
-                        elif action == "next_move_b":
-                            GAMES[game_id]["type"] = "prev_move_b"
-                            print(payload)
-                        else:
-                            GAMES[game_id] = {
-                                "type": "unmatch",
-                                "game_id": game_id,
-                                "websocket_id": websocket_id,
-                            }
+                if game_id != "" and (action := payload.get("action")):
+                    message: dict[str, Any] = {}
 
-                    await websocket.send_json(GAMES[game_id])
+                    # if action == "select_cards":
+                    #     message = {
+                    #         "type": "reload",
+                    #         "game_id": game.uniqid,
+                    #         "board": {
+                    #             "rules": {},
+                    #             "hold": {
+                    #                 "a": game.random_cards(Hand),
+                    #                 "b": get_random_cards("b"),
+                    #             },
+                    #             "ondeck": [],
+                    #             "score": {
+                    #                 "a": 0,
+                    #                 "b": 0,
+                    #             },
+                    #             "turn": random.choice(["B", "R"]),
+                    #         },
+                    #     }
+                    # elif game_id in GAMES:
+                    #     if action == "start-game":
+                    #         GAMES[game_id]["type"] = "distribute"
+                    #     elif action == "next_turn_a":
+                    #         GAMES[game_id]["type"] = "prev_move_a"
+                    #         print(payload)
+                    #     elif action == "next_move_b":
+                    #         GAMES[game_id]["type"] = "prev_move_b"
+                    #         print(payload)
+                    #     else:
+                    #         GAMES[game_id] = {
+                    #             "type": "unmatch",
+                    #             "game_id": game_id,
+                    #             "websocket_id": websocket_id,
+                    #         }
+
+                    await websocket.send_json(message)
                 await asyncio.sleep(1 / 30)
             except WebSocketDisconnect:
                 try:
